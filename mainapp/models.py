984a063438983acebaa393b91314cde065476174
alistvt <alistvt@gmail.com>
2019-02-06 18:26:50 +0330

    barber model class created

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Barber(models.Model):
-    pass
+class Barbery(models.Model):
+    user = models.OneToOneField(User, related_name='barbery', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=1000, unique=True)
+    createdDate = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True, blank=True)
 
 class UserProfile(models.Model):
-    pass
+	pass
 
 class TimeSlot(models.Model):
     pass
