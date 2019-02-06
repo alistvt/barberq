25b899573368a67d7a75b3d9de2853b70e856794
alistvt <alistvt@gmail.com>
2019-02-06 18:44:30 +0330

    userprofile model created and migrated

--- mainapp/models.py
+++ mainapp/models.py
@@ -8,3 +8,7 @@ class Barbery(models.Model):
     address = models.CharField(max_length=1000, unique=True)
     createdDate = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True, blank=True)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
+    
