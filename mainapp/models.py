8af80710c3a3ea50ab44c6c7758e251253b4c580
alistvt <alistvt@gmail.com>
2019-02-06 18:15:20 +0330

    model names created

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Barber(models.Model):
+    pass
+
+class UserProfile(models.Model):
+    pass
+
+class TimeSlot(models.Model):
+    pass
+
+class Reservation(models.Model):
+    pass
