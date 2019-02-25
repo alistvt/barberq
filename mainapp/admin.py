71d4a3af73bddc8964d55012f55f156a1cf7077a
alistvt <alistvt@gmail.com>
2019-02-25 17:18:20 +0330

    creates skeleton of the admin classes

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Barbery, UserProfile, TimeSlot, Reservation
 # Register your models here.
+
+
+class BarberyAdmin(admin.ModelAdmin):
+    pass
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+class TimeSlotAdmin(admin.ModelAdmin):
+    pass
+
+
+class ReservationAdmin(admin.ModelAdmin):
+    pass
