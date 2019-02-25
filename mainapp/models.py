fce607b68641a4c8e458d029b662ebfd22efaf88
alistvt <alistvt@gmail.com>
2019-02-25 17:03:54 +0330

    adds str methods to models

--- mainapp/models.py
+++ mainapp/models.py
@@ -11,9 +11,15 @@ class Barbery(User):
     created_date = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True, blank=True)
 
+    def __str__(self):
+        return '{name}'.format(name=self.name)
+
 
 class UserProfile(User):
     pass
+
+    def __str__(self):
+        return '{first_name} {last_name}'.format(first_name=self.first_name, last_name=self.last_name)
 
 
 class TimeSlot(models.Model):
@@ -23,8 +29,14 @@ class TimeSlot(models.Model):
     duration = models.DurationField(default=timedelta(hours=1))
     reserved = models.BooleanField(default=False)
 
+    def __str__(self):
+        return '{barbery}@{date}'.format(barbery=self.barbery, date=self.start_time)
+
 
 class Reservation(models.Model):
     user = models.ForeignKey(UserProfile, related_name='reservations', on_delete=models.CASCADE)
     slot = models.OneToOneField(TimeSlot, related_name='reservation', on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{user}-{slot}'.format(user=self.user, slot=self.slot)
