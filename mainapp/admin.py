2af38e6deae08ceefb67c40e8b4a80c02b3a648e
alistvt <alistvt@gmail.com>
2019-02-25 23:02:03 +0330

    creates skleton of the UserProfileAdmin class

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -2,27 +2,33 @@ from django.contrib import admin
 from .models import Barbery, UserProfile, TimeSlot, Reservation
 # Register your models here.
 
+
 class BarberyAdmin(admin.ModelAdmin):
+    #todo
     # Model Admins inherited from user model, should always exclude some fields, specially password (it's always excluded in our admins)
     # In order to create a better profile admin, take a look at django user admin, specially these two lines:
 
     # form = UserChangeForm
     # add_form = UserCreationForm
 
-    # Take a look at django admin form documentation
     exclude = ('password',)
     fields = ('name', ('first_name', 'last_name'), 'email', 'address', 'is_active',)
     list_display = ('name', 'first_name', 'last_name', 'address', 'is_active', )
     search_fields = ('name', 'first_name', 'last_name', 'address',)
-    #todo fix
+
     # We user list filter for fields with enumerated values, with less than around 10 choices. (e.g. gender).
     # Date or Time fields are not good for list filter. Use date_hierarchy instead
+
     list_filter = ('is_active', )
     ordering = ('name',)
 
 
 class UserProfileAdmin(admin.ModelAdmin):
-    pass
+    fields = ('email', ('first_name', 'last_name'), 'is_active',)
+    list_display = ('email', 'first_name', 'last_name', 'is_active', )
+    search_fields = ('email', 'first_name', 'last_name',)
+    list_filter = ('is_active', )
+    ordering = ('email',)
 
 
 class TimeSlotAdmin(admin.ModelAdmin):
