8c1e76195f560b1d8222f65e2060c961e202cdb1
alistvt <alistvt@gmail.com>
2019-02-25 16:50:37 +0330

    changes the code style from 1-to-1 relationship to inheritance style

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,16 +5,15 @@ from django.db import models
 
 
 # Create your models here.
-class Barbery(models.Model):
-    barber = models.OneToOneField(User, related_name='barbery', on_delete=models.CASCADE)
+class Barbery(User):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=1000, unique=True)
     created_date = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True, blank=True)
 
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
+class UserProfile(User):
+    pass
 
 
 class TimeSlot(models.Model):
