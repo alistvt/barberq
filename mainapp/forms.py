82edf3aea0c3158e3f3480676f8f6ce7dc816b09
alistvt <alistvt@gmail.com>
2019-03-02 18:57:06 +0330

    creates the ReservationForm

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,1 +1,8 @@
-#
+from django.forms import ModelForm
+from mainapp.models import Reservation
+
+
+class ReservationForm(ModelForm):
+    class Meta:
+        model = Reservation
+        fields = ['user', 'slot', ]
