6b00c1f14787ead24e69ce7c30b08bd300d99dd6
alistvt <alistvt@gmail.com>
2019-03-08 01:17:37 +0330

    creates urls of that 4 views of barber

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^login/$', views.barber_login, name='barber_login'),
     url(r'^logout/$', views.barber_logout, name='barber_logout'),
     url(r'^profile/$', views.barber_profile, name='barber_profile'),
+    url(r'^reserves/$', views.view_reserves, name='view_reserves'),
+    url(r'^addslot/$', views.add_slot, name='add_slot'),
+    url(r'^manageslots/$', views.manage_slots, name='manage_slots'),
 ]
