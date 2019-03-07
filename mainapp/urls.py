9a121e7eb5beb436aefcd5fb4d8fd1cc242d7664
alistvt <alistvt@gmail.com>
2019-03-07 06:23:59 +0330

    adds urls for logout and barber profile

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,4 +4,6 @@ from mainapp import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^login/$', views.barber_login, name='barber_login'),
+    url(r'^logout/$', views.barber_logout, name='barber_logout'),
+    url(r'^profile/$', views.barber_profile, name='barber_profile'),
 ]
