a9981b67a2da1d037ee042a102b503bfd94a1cf4
alistvt <alistvt@gmail.com>
2019-04-13 17:38:01 +0430

    change in the urls and moving barber to the barbers app

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,15 +3,15 @@ from mainapp import views
 from mainapp.views import api
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^login/$', views.barber_login, name='barber_login'),
-    url(r'^logout/$', views.barber_logout, name='barber_logout'),
-    url(r'^profile/$', views.barber_profile, name='barber_profile'),
-    url(r'^reserves/$', views.view_reserves, name='view_reserves'),
-    url(r'^addslot/$', views.add_slot, name='add_slot'),
-    url(r'^manageslots/$', views.manage_slots, name='manage_slots'),
+    url(r'^barbers/$', views.home, name='home'),
+    url(r'^barbers/login/$', views.barber_login, name='barber_login'),
+    url(r'^barbers/logout/$', views.barber_logout, name='barber_logout'),
+    url(r'^barbers/profile/$', views.barber_profile, name='barber_profile'),
+    url(r'^barbers/reserves/$', views.view_reserves, name='view_reserves'),
+    url(r'^barbers/addslot/$', views.add_slot, name='add_slot'),
+    url(r'^barbers/manageslots/$', views.manage_slots, name='manage_slots'),
 
     url(r'^api/barbers/list/$', api.BarberyListView.as_view(), name='barber_list'),
+    url(r'^api/barbers/profile/$', api.BarberyListView.as_view(), name='barber_list'),
 
 ]
-
